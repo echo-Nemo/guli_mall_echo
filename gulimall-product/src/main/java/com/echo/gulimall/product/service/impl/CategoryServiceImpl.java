1eaf9fe9338e4caf7c9d93c295db4d6de3fd182d
echo <1342525123@qq.com>
2022-01-12 23:29:26 +0800

    3级菜单的提交

--- gulimall-product/src/main/java/com/echo/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/echo/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.echo.gulimall.product.service.impl;
 
+import lombok.val;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +31,25 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public PageUtils listWithTree() {
+        //先查出所有分类
+        List<CategoryEntity> rootCategoryList = baseMapper.selectList(null);
+        // 获取该菜单下的一级分类
+        List<CategoryEntity> firstCategoryList = rootCategoryList.stream().filter(categoryEntity -> categoryEntity
+                .getParentCid() == 0).collect(Collectors.toList());
+
+        //firstCategoryList.stream().map(categoryEntity ->)
+        return null;
+    }
+
+    // 递归查出该分类下的子分类
+    public List<CategoryEntity> getChildren(CategoryEntity rootCategory, List<CategoryEntity> allCategory) {
+        // 改菜单下的所有子菜单
+        List<CategoryEntity> parentCategoryList = allCategory.stream().filter(categoryEntity -> categoryEntity.getParentCid().longValue() == rootCategory.getCatId()).collect(Collectors.toList());
+//         parentCategoryList.stream().map(categoryEntity -> getChildren(categoryEntity, allCategory)).sorted((menu1,menu2)->{
+//             me
+//         })
+        return null;
+    }
 }
